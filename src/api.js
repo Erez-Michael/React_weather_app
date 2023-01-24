5ca0e30021a9504b87f495aeae859681c6a61997
erezmichael <erezmichael01@gmail.com>
2023-01-23 21:20:02 -0500

    First push

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "63d64b0784msh91b66516d9d87d2p16bffcjsnefa4773626ce",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "43d55faf2ff65d5f45ee69586a6af094";
